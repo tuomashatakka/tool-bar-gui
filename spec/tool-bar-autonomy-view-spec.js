e91f92e9f455d880b4f968cda0d1f1b270ba2a3e
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-03-12 20:13:33 +0200

    Initial commit

---
+++ spec/tool-bar-autonomy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ToolBarAutonomyView from '../lib/tool-bar-autonomy-view';
+
+describe('ToolBarAutonomyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
